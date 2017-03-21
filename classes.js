c3cf1e2770d7f72c0fb94858630ea039a6507aea
zac-metin <zac.metin@gmail.com>
2017-03-21 13:19:40 +1300

    makeVisible

--- classes.js
+++ classes.js
@@ -1,25 +1,28 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+one()
+two()
+three()
+makeVisible()
 }
 
 function one () {
-  // First, we have to find the element:
   var one = document.getElementById('one')
-
-  // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
-
-// CREATE FUNCTION two HERE
-
-// CREATE FUNCTION three HERE
-
-// CREATE FUNCTION makeVisible HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('orange')
+}
+function makeVisible () {
+  var invisible = document.getElementsByClassName('invisible')
+  invisible[0].classList.add('visible')
+}
